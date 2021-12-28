fad0d5c8f1da759e8e1fd54e90dff38fa77797f4
It's Me <58089002+NabilahSharfina@users.noreply.github.com>
2021-12-28 23:36:33 +0700

    Delete PRAKTIKUM/Pertemuan 6 directory

--- PRAKTIKUM/Pertemuan 6/Praktek/create_message.php
+++
@@ -1,9 +0,0 @@
-<?php
-    function create_message($text,$type,$icon){
-        session_start();
-        $_SESSION["message"]['text'] = $text;
-        $_SESSION["message"]['type'] = $type;
-        $_SESSION["message"]['icon'] = $icon;
-        $_SESSION["message"]['show'] = "show";
-    }
-?>
--- PRAKTIKUM/Pertemuan 6/README.md
+++
@@ -1,8 +0,0 @@
-|  | NIM | NAME | CLASS | SEMESTER |
-| - | - | - | - | - |
-| 👩‍🎓 | 19104025 | Nabilah Sharfina | S1SE-03-A | 5 |
-
-# The Result from "Open in Browser"
-A. PRACTICE
-
-B. TASK
